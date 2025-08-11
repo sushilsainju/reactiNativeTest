9f7719ddbfa5c069c663349efd2af4fe6b8184b2
Sushil Sainju <suhil.sainju@gmail.com>
2025-08-11 00:40:22 -0500

    Trending View

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,20 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "react-native";
 import "./globals.css"; // Import global styles
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen
-        name="(tabs)"
-        options={{ title: "Saved", headerShown: false }}
-      />
-      <Stack.Screen
-        name="movies/[id]"
-        options={{ title: "Movie Details", headerShown: false }}
-      />
-    </Stack>
+    <>
+      <StatusBar hidden={true} />
+      <Stack>
+        <Stack.Screen
+          name="(tabs)"
+          options={{ title: "Saved", headerShown: false }}
+        />
+        <Stack.Screen
+          name="movies/[id]"
+          options={{ title: "Movie Details", headerShown: false }}
+        />
+      </Stack>
+    </>
   );
 }
